617fc29b520c3e2a37450d7dbf5936d8531d1aee
Razka173 <44521555+Razka173@users.noreply.github.com>
2019-09-21 20:59:52 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import urllib.request, json
 from urllib.parse import unquote
 import os
 import sys
-
 import random
 
 # Discord Library
@@ -14,13 +13,14 @@ from pypresence import Presence
 import time
 
 # For local running
-from dotenv import load_dotenv
-load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+# from dotenv import load_dotenv
+# load_dotenv()
+# token = os.getenv('DISCORD_TOKEN')
+# GUILD = os.getenv('DISCORD_GUILD')
 
-# for heroku running, put your token into environment variable
+# For heroku running, put your token into environment variable
 #token = sys.argv[1]
+token = os.environ.get('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
@@ -37,24 +37,16 @@ async def on_member_join(member):
 
 @bot.command(name='cek')
 async def cek_harga(ctx, *, keyword):
-    print("masuk fungsi cek harga")
-    print(keyword)
-    print(type(keyword))
     var_item = urllib.parse.quote(keyword)
-    print(var_item)
     var_slim = "&slim=true"
     var_exact = "&exact=false"
     link = "https://www.romexchange.com/api?item=" + var_item + var_slim + var_exact
-
-    #print(link)
     with urllib.request.urlopen(link) as url:
-        #print("masuk request")
         data = json.loads(url.read().decode())
         arraydata = list()
         if len(data) == 0:
             response = "No Item Found"
         else:
-            #print("masuk proses pencarian")
             for i in range (len(data)):
                 nama_barang = data[i].get("name")
                 harga_sea = data[i].get("sea").get("latest") #int
@@ -69,13 +61,11 @@ async def cek_harga(ctx, *, keyword):
             if arraydata == None:
                 response = "No Item Found"
             else:
-                #print("masuk tahap akhir proses")
                 response = ""
                 for data in arraydata:
                     response += "**__" + data.get("nama_barang") + "__**" + "\n"
                     response += "SEA Price: " + data.get("harga_sea")
                     response += " (" + data.get("week_change") + " this week)" + "\n \n"
-                #print(response)
         print("operation success")
         await ctx.send(response)
 
@@ -93,4 +83,3 @@ async def on_command_error(ctx, error):
         await ctx.send('You do not have the correct role for this command.')
 
 bot.run(token)
-# client.run(token)
