6873224dc8b32fed87ca7da2f28da80d3cd14b26
DonatSeres <44521555+Razka173@users.noreply.github.com>
2019-11-15 17:37:42 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,37 +1,43 @@
-import urllib.request, json
+import urllib.request
+import json
 from urllib.parse import unquote
 import os
-import sys
+#import sys
 import random
-
-# For local running
+import datetime
+# discord library
+import discord
+from discord.ext import commands
+# local Running Library
 from dotenv import load_dotenv
 load_dotenv()
 
-# Discord Library
-import discord
-from discord.ext import commands
-
-# For heroku running
+# get token
 try:
-    print("success get token")
     token = os.environ.get('DISCORD_TOKEN')
+    print("success get token")
 except:
-    print("This bot running from localhost")
     token = os.getenv('DISCORD_TOKEN')
+    print("This bot running from localhost")
 
-# Creating bot variable
-bot = commands.Bot(command_prefix='!')
+
+# instance object called 'bot'
+bot_prefix = '!'
+bot = commands.Bot(command_prefix=bot_prefix)
 bot.description = "Bot for find price on http://www.romexchange.com"
-bot.activity = discord.Activity(name="!cek", detail="!cek", type=discord.ActivityType.listening)
+bot.activity = discord.Activity(name="!cek", detail="!cek", type=discord.ActivityType.listening, start=datetime.datetime.now())
 
 pesan = commands.DefaultHelpCommand(dm_help=True)
 bot.help_command = pesan
 
+# Fungsi untuk callback jika bot berhasil koneksi ke discord server
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} is connected')
-
+    # for guild in bot.guilds:
+    #     print(guild, guild.member_count)
+    
+# Fungsi untuk mengirim pesan ketika member bergabung ke dalam server
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
@@ -39,6 +45,7 @@ async def on_member_join(member):
         f'Hi {member.name}, welcome to my Discord server!'
     )
 
+# Fungsi untuk mengambil harga barang dari website romexchange
 @bot.command(name='cek')
 async def cek_harga(ctx, *, keyword):
     var_item = urllib.parse.quote(keyword)
@@ -73,6 +80,7 @@ async def cek_harga(ctx, *, keyword):
         print("operation success")
         await ctx.send(response)
 
+# Fungsi untuk error reporting
 @bot.event
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
@@ -81,9 +89,37 @@ async def on_error(event, *args, **kwargs):
         else:
             raise
 
+# Fungsi untuk error reporting
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
+
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(title="SmokieBot", description=bot.description, color=0xeee657)
+
+    # give info about you here
+    embed.add_field(name="Author", value="Donat#4054")
+
+    # Shows the number of servers the bot is member of.
+    embed.add_field(name="Server count", value=f"{len(bot.guilds)}")
+
+    # give users a link to invite thsi bot to their server
+    embed.add_field(name="Invite", value="[Add SmokieBot](https://discordapp.com/api/oauth2/authorize?client_id=618658270323933199&permissions=251968&scope=bot)")
+
+    await ctx.send(embed=embed)
+
+bot.remove_command('help')
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="Smokie bot", description=bot.description+". List of commands are:", color=0xeee657)
+    
+    embed.add_field(name=bot_prefix+"cek [item-name]", value="Find price of items with given names (example: "+bot_prefix+"cek minorous card)", inline=False)
+    embed.add_field(name=bot_prefix+"info", value="Gives a little info about the bot", inline=False)
+    embed.add_field(name=bot_prefix+"help", value="Gives this message", inline=False)
+
+    await ctx.author.send(embed=embed)
+    await ctx.send(embed=embed)
 
 bot.run(token)
